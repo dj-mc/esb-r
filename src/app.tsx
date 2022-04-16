bba7db08fa7677ba36561ef0c4d7185ffe9a7ac0
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-04-15 21:10:06 -0500

    Compose components, inline styling

--- src/app.tsx
+++ src/app.tsx
@@ -4,25 +4,60 @@ import Server from 'react-dom/server';
 import Game from './tic-tac-toe';
 import LikeButton from './like-button';
 
-const Greet = () => {
+const Greet = (props: { name: string; status: string }) => {
   return (
     <div>
       <h1>esb + react</h1>
+      <p>
+        Hello, {props.name}.
+        <br />
+        The time is {Date.now()}.
+        <br />
+        Your status is: {props.status}.
+      </p>
+    </div>
+  );
+};
+
+const greet_options = {
+  name: 'dj-mc',
+  status: 'Programming'
+};
+
+const MyFooter = () => {
+  return (
+    <>
+      <footer>Thanks for being apart of the world wide web!</footer>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Greet {...greet_options} />
+      <h1>Main content</h1>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias ipsum
         dignissimos ipsa. Rerum debitis quidem veniam natus, perspiciatis
         soluta, ex molestias, commodi dolorem placeat tenetur. Distinctio
         excepturi necessitatibus iusto facilis?
       </p>
-    </div>
+      <Game />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center'
+        }}
+      >
+        <LikeButton />
+        <MyFooter />
+      </div>
+    </>
   );
 };
 
-console.log(Server.renderToString(<Greet />));
+console.log(Server.renderToString(<Greet {...greet_options} />));
 
-ReactDOM.render(<Game />, document.getElementById('game_container'));
-
-ReactDOM.render(
-  <LikeButton />,
-  document.querySelector('#like_button_container')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/like-button.tsx
+++ src/like-button.tsx
@@ -17,7 +17,7 @@ export default class LikeButton extends React.Component<unknown, IState> {
 
     return React.createElement(
       'button',
-      { onClick: () => this.setState({ liked: true }) },
+      { id: 'like-btn', onClick: () => this.setState({ liked: true }) },
       'Like'
     );
   }
