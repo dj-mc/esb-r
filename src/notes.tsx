f14f4cda6327c043656a6eae1e956cba183718ce
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-05-20 15:30:12 -0500

    Concat a new object to state

--- src/notes.tsx
+++ src/notes.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ButtonOnClick } from './button-on-click';
 import { INote, TNoteList } from './notes-data';
 
 const MakeNoteLi = (props: { notes: TNoteList }) => {
@@ -12,12 +13,47 @@ const MakeNoteLi = (props: { notes: TNoteList }) => {
 };
 
 export const Notes = (props: { notes: TNoteList }) => {
+  const [notes, set_notes] = useState(props.notes);
+  const [new_note, set_new_note] = useState('');
+  const [display_all, set_display_all] = useState(true);
+
+  const add_note = (e: Event) => {
+    e.preventDefault();
+    console.log('Clicked me', e.target);
+
+    const note_obj = {
+      id: notes.length + 1,
+      content: new_note,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    };
+
+    set_notes(notes.concat(note_obj));
+    set_new_note('');
+  };
+
+  const input_change = (e: Event & { target: HTMLInputElement }) => {
+    set_new_note(e.target.value);
+  };
+
+  const display_these = display_all
+    ? notes
+    : notes.filter((note) => note.important);
+
   return (
     <>
       <h1>Notes</h1>
+      <ButtonOnClick
+        fn={() => set_display_all(!display_all)}
+        text={display_all ? 'important' : 'all'}
+      />
       <ul>
-        <MakeNoteLi notes={props.notes} />
+        <MakeNoteLi notes={display_these} />
       </ul>
+      <form onSubmit={add_note}>
+        <input value={new_note} onChange={input_change} />
+        <button type="submit">Save New Note</button>
+      </form>
     </>
   );
 };
