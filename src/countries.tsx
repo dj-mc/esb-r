1bc340915d45bb1ac448eedfe311693bbc9c14f3
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-05-30 16:51:39 -0500

    Add search function for Countries (10 limit)

--- src/countries.tsx
+++ src/countries.tsx
@@ -20,9 +20,29 @@ const MakeCountryLi = (props: { countries: TCountryList }) => {
   );
 };
 
+const DisplaySearch = (props: { search: string; countries: TCountryList }) => {
+  const { search, countries } = props;
+  const searched_countries: TCountryList = [];
+
+  countries.map((each: ICountry) => {
+    const found_common = each.name.common
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    const found_official = each.name.official
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    if (search.length > 0 && (found_common || found_official)) {
+      if (searched_countries.length < 10) searched_countries.push(each);
+    }
+  });
+
+  return <MakeCountryLi countries={searched_countries} />;
+};
+
 export const Countries: React.FC = () => {
   const init_countries: TCountryList = [];
   const [countries, set_countries] = useState(init_countries);
+  const [search, set_search] = useState('');
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all').then((response) => {
@@ -30,9 +50,20 @@ export const Countries: React.FC = () => {
     });
   }, []);
 
+  type TEvent = React.ChangeEvent<HTMLInputElement>;
+
+  const input_change_search = (e: TEvent) => {
+    set_search(e.target.value);
+  };
+
   return (
     <>
-      <MakeCountryLi countries={countries} />
+      <h2>Countries</h2>
+      Search:
+      <input value={search} onChange={input_change_search} />
+      <ul>
+        <DisplaySearch search={search} countries={countries} />
+      </ul>
     </>
   );
 };
